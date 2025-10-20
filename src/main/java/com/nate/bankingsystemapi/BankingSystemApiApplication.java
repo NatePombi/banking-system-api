b56f98d570809ea4990df065e64313754d8332ab
Nathan Pombi <pombinathan@gmail.com>
2025-10-21 00:53:16 +0200

    Initial commit

---
+++ src/main/java/com/nate/bankingsystemapi/BankingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.nate.bankingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSystemApiApplication.class, args);
+    }
+
+}
