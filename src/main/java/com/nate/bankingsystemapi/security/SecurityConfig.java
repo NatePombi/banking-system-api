1a05891d3dd789635176703e11dbf81036294f13
Nathan Pombi <pombinathan@gmail.com>
2026-01-12 00:45:35 +0200

    Fix: Fix CORS using allowedOriginPatterns for Railway

--- src/main/java/com/nate/bankingsystemapi/security/SecurityConfig.java
+++ src/main/java/com/nate/bankingsystemapi/security/SecurityConfig.java
@@ -46,15 +46,19 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://banking-system-api-production.up.railway.app"));
+
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "https://*.railway.app"
+        ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
