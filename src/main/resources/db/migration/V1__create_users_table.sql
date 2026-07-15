d4d0895cdb090720205db6d70fdbc33e90a4615d
Nathan Pombi <pombinathan@gmail.com>
2026-07-15 02:38:17 +0200

    refactor(database): align Flyway migrations with domain model

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,7 +1,13 @@
+create TYPE user_status as enum('ACTIVE','LOCKED');
+create TYPE user_role as enum('USER','ADMIN');
+
+
 CREATE TABLE users (
                        id BIGSERIAL PRIMARY KEY,
                        full_Name VARCHAR(50) NOT NULL,
                        username VARCHAR(50) UNIQUE NOT NULL,
                        password VARCHAR(255) NOT NULL,
-                       role VARCHAR(10) NOT NULL
+                       role user_role NOT NULL default 'USER',
+                       status user_status not null default 'ACTIVE',
+                       created_at TIMESTAMP not null
 );
--- src/main/resources/db/migration/V2__create_accounts_table.sql
+++ src/main/resources/db/migration/V2__create_accounts_table.sql
@@ -1,8 +1,15 @@
-CREATE TABLE account (
+create TYPE account_status as enum('ACTIVE','SUSPENDED');
+
+CREATE TABLE accounts (
                          id BIGSERIAL PRIMARY KEY,
-                         account_number BIGINT NOT NULL ,
-                         balance BIGINT NOT NULL,
+                         account_number BIGINT NOT NULL unique ,
+                         balance DECIMAL(12,2) NOT NULL,
                          currency VARCHAR(3) NOT NULL,
                          user_id BIGINT REFERENCES users(id),
-                         created_at TIMESTAMP with time zone not null
+                         status account_status not null default 'ACTIVE',
+                         created_at TIMESTAMP with time zone not null,
+
+                         CONSTRAINT uk_user_currency unique (user_id,currency)
 );
+
+create INDEX idx_account_user_id on accounts(user_id);
